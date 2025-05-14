cec49847a0b1efafd0eadb1c2c5e4c4d01715eeb
unknown <fahzzzstd04@gmail.com>
2025-05-14 11:19:31 +0700

    menambahkan 3 file source2

---
+++ Flora.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ulasan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Flora {
+// property / dikenal dengan variabel
+// method   
+    
+    String nama="Kelengkeng",jenisAkar,batang;
+    int usia;
+    
+}
